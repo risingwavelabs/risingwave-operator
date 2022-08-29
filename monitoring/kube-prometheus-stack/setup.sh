ff78b2184739b1c7601e3c04ba3179b048e2213c
Gautam Manek <gautammanek@Gautams-MacBook-Pro.local>
2022-08-29 20:28:11 +0800

    feat: add script for local dev

---
+++ monitoring/kube-prometheus-stack/setup.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+#
+# Copyright 2022 Singularity Data
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# install the necessary helm charts if not installed
+
+_SCRIPT_BASEDIR=$(dirname "$0")
+CLUSTER_NAME="onebox"
+cluster="kind"
+RISINGWAVE_OPERATOR_NAMESPACE="risingwave-operator-system"
+PROMETHEUS_NAMESPACE="default"
+PROMETHEUS_RELEASE_NAME="prometheus"
+REMOTE_WRITE_ADAPTER_NAME="prometheus-remote-write-adapter"
+ZOOKEEPER_NAME="zkp"
+KAFKA_NAME="kstack"
+KAFKA_UI_NAME="kafka-ui"
+KAFKA_UI_PORT=8081
+MONITORING_TENANT_NAME="monitoring"
+
+if [ "$(kind get clusters | grep ${CLUSTER_NAME})" == "" ]; then
+    echo "Ensure the cluster ${CLUSTER_NAME} is up and running!"
+    exit 1
+fi 
+
+if ! [ -x "$(command -v rwc 2>&-)" ]; then
+    echo "Ensure that the rwc cli is installed"
+    exit 1
+fi
+
+if ! kubectl get secrets | grep -E '^aws-prometheus-credentials'>/dev/null; then
+    echo "Ensure that the secret aws-prometheus-credentials is set"
+    exit 1
+fi
+
+load_image_in_cluster() {
+    set -e 
+    repo=$1 
+    tag=$2
+    if [[ "$(docker images -q $repo:$tag 2> /dev/null)" == "" ]]; then
+        echo "Pulling image: ${repo}:${tag}"
+        docker pull $repo:$tag
+    fi
+    kind load docker-image ${repo}:${tag} --name ${CLUSTER_NAME}
+    set +e
+}
+
+### Deploy Promtheus if not deployed ###
+if [ "$(kubectl get pods --namespace "${PROMETHEUS_NAMESPACE}" | grep "${PROMETHEUS_RELEASE_NAME}")" == "" ]; then
+    echo "Creating prometheus deployment..."
+    helm upgrade --install "${PROMETHEUS_RELEASE_NAME}" prometheus-community/kube-prometheus-stack \
+    -f https://raw.githubusercontent.com/singularity-data/risingwave-operator/main/monitoring/kube-prometheus-stack/kube-prometheus-stack.yaml \
+    -f "${_SCRIPT_BASEDIR}"/prometheus-remote-write-aws.yaml
+fi
+
+### Deploy Read Write adapter if not deployed ###
+if [ "$(kubectl get pods --namespace "${RISINGWAVE_OPERATOR_NAMESPACE}" | grep "${REMOTE_WRITE_ADAPTER_NAME}")" == "" ]; then
+    echo "Creating adapter deployment..."
+    kubectl apply -f "${_SCRIPT_BASEDIR}"/remote-write-adapter.yaml
+fi
+
+### Deploy zookeeper if not deployed ###
+if [ "$(kubectl get pods --namespace ${RISINGWAVE_OPERATOR_NAMESPACE} | grep ${ZOOKEEPER_NAME})" == "" ]; then
+    echo "Loading zookeeper in cluster..."
+    helm upgrade --install "${ZOOKEEPER_NAME}" --namespace ${RISINGWAVE_OPERATOR_NAMESPACE} -f ./zookeeper-deployment.yaml rhcharts/zookeeper --wait --timeout 20m
+fi
+
+### Connect kafka to zookeeper ###
+if [ "$(kubectl get pods --namespace "${RISINGWAVE_OPERATOR_NAMESPACE}" | grep "${KAFKA_NAME}")" == "" ]; then
+    echo "Loading kafka in cluster..."
+    helm upgrade --install "${KAFKA_NAME}" --namespace "${RISINGWAVE_OPERATOR_NAMESPACE}" -f ./kafka-deployment.yaml rhcharts/kafka --wait --timeout 20m
+fi
+
+### Install kafka ui for monitoring ###
+if [ "$(kubectl get pods --namespace "${RISINGWAVE_OPERATOR_NAMESPACE}" | grep "${KAFKA_UI_NAME}")" == "" ]; then
+    echo "Loading kafka UI in cluster..."
+    helm upgrade --install "${KAFKA_UI_NAME}" kafka-ui/kafka-ui -f kafka-ui.yaml --namespace="${RISINGWAVE_OPERATOR_NAMESPACE}" --wait --timeout 2m
+    ulimit -n 65536	
+    echo "You can access the kafka ui at port "${KAFKA_UI_PORT}""
+fi
+
+if [ "$(rwc t get -tenant "${MONITORING_TENANT_NAME}" 2>&1 | grep '401')" != "" ]; then
+    echo "Login required..."
+    rwc config -region local
+    if [ $? -ne 0 ]; then 
+        echo "Could not configure the rwc cli instance"
+        exit 1
+    fi
+    echo "Configured rwc cli instance"
+    PS3="Have you registered for a local account yet: "
+    select registered in yes no; do
+        read -p "Enter your username: " rwc_username
+        read -s -p "Enter your password: " rwc_password
+        case $registered in
+            yes)
+                rwc login -account ${rwc_username} -password ${rwc_password}
+                break;;
+            no) 
+                rwc register -account ${rwc_username} -password ${rwc_password}
+                break;;
+            *)
+                echo "Invalid option: $REPLY";;
+        esac
+    done  
+    if [ $? -ne 0 ]; then
+        echo "Could not login to the risingwave cli"
+        exit 1 
+    fi
+fi
+
+if [ "$(kubectl get pods --namespace "${MONITORING_TENANT_NAME}")" != "" ]; then
+    echo "existing monitoring tenant detected. cleaning up"
+    kubectl delete --all pods --namespace="${MONITORING_TENANT_NAME}"
+    kubectl delete --all deployments --namespace="${MONITORING_TENANT_NAME}"
+    echo "cleaning up existing resources..."
+    kubectl wait --for delete --all pods --namespace="${MONITORING_TENANT_NAME}" --timeout=5m
+    kubectl wait --for delete --all deployments --namespace="${MONITORING_TENANT_NAME}" --timeout=5m
+    if [ $? -ne 0 ]; then
+        echo "Could not delete all deployments in namespace ${MONITORING_TENANT_NAME}"
+        exit 1 
+    fi
+fi
+echo "creating monitoring tenant..."
+rwc -t create -tenant "${MONITORING_TENANT_NAME}"
+kubectl wait --for=condition=Ready pods --namespace "${MONITORING_TENANT_NAME}" --timeout=5m
+psql "$(rwc t get-endpoint -tenant "${MONITORING_TENANT_NAME}")"
+if [ $? -ne 0 ]; then
+    echo "Could not connect to tenant "${MONITORING_TENANT_NAME}""
+    exit 1 
+fi
+# kubectl port-forward --namespace risingwave-operator-system svc/kafka-ui "${KAFKA_UI_PORT}":80
+
+
+
+

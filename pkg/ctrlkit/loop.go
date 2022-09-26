feff4ee03336d6d37d34eccb1f99e236af31b88d
CAJan93 <jan.mensch@gmx.net>
2022-09-26 15:14:04 +0200

    undo changes in loop.go

--- pkg/ctrlkit/loop.go
+++ pkg/ctrlkit/loop.go
@@ -21,8 +21,6 @@ import (
 	"time"
 
 	ctrl "sigs.k8s.io/controller-runtime"
-
-	m "github.com/risingwavelabs/risingwave-operator/pkg/metrics"
 )
 
 // NeedsRequeue reports if the result and error indicates a requeue.
@@ -32,32 +30,25 @@ func NeedsRequeue(result ctrl.Result, err error) bool {
 
 // RequeueImmediately returns a result with requeue set to true and a nil.
 func RequeueImmediately() (ctrl.Result, error) {
-	m.ControllerReconcileRequeueCount.Inc()
 	return ctrl.Result{Requeue: true}, nil
 }
 
 // RequeueAfter returns a result with requeue after set to the given duration and a nil.
 func RequeueAfter(after time.Duration) (ctrl.Result, error) {
-	m.ControllerReconcileRequeueAfter.Set(float64(after.Milliseconds()))
-	m.ControllerReconcileCount.Inc()
 	return ctrl.Result{RequeueAfter: after}, nil
 }
 
 // RequeueIfError returns an empty result with the err.
 func RequeueIfError(err error) (ctrl.Result, error) {
-	m.ControllerReconcileRequeueErrorCount.Inc()
 	return ctrl.Result{}, err
 }
 
 // RequeueIfErrorAndWrap returns an empty result with a wrapped err.
 func RequeueIfErrorAndWrap(explain string, err error) (ctrl.Result, error) {
 	if err != nil {
-		m.ControllerReconcileRequeueErrorCount.Inc()
 		return ctrl.Result{}, fmt.Errorf("%s: %w", explain, err)
-	} else {
-		m.ControllerReconcileRequeueCount.Inc()
-		return ctrl.Result{}, nil
 	}
+	return ctrl.Result{}, nil
 }
 
 // NoRequeue returns an empty result and a nil.

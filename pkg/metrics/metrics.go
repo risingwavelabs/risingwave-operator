aa3706f25ef76ccc7a6ca232a01bc42cf79d2b79
CAJan93 <jan.mensch@gmx.net>
2022-09-27 11:13:47 +0200

    minor change

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -118,6 +118,12 @@ var (
 		},
 		[]string{"group", "version", "kind", "namespace", "name"},
 	)
+	controllerReconcileDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "controller_reconcile_duration",
+		Help: "Length of time per reconciliation per controller",
+		Buckets: []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
+			1.25, 1.5, 1.75, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 40, 50, 60},
+	}, []string{"controller", "group", "version", "kind", "namespace", "name"})
 	controllerReconcilePanicCount = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "controller_reconcile_panic_count",
@@ -125,12 +131,6 @@ var (
 		},
 		[]string{"group", "version", "kind", "namespace", "name"},
 	)
-	controllerReconcileDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name: "controller_reconcile_duration",
-		Help: "Length of time per reconciliation per controller",
-		Buckets: []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0,
-			1.25, 1.5, 1.75, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 40, 50, 60},
-	}, []string{"controller", "group", "version", "kind", "namespace", "name"})
 )
 
 // rwReqData hold some fields of the RisingWave request.

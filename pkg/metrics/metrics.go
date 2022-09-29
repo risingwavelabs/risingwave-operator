02e6aa648c7c082b398f2205e8965ee24fdd895e
CAJan93 <jan.mensch@gmx.net>
2022-09-29 15:27:15 +0200

    add comments

--- pkg/metrics/metrics.go
+++ pkg/metrics/metrics.go
@@ -146,10 +146,12 @@ func NewWebhookTypes(isValidating bool) WebhookType {
 	return WebhookType{isValidating_: isValidating}
 }
 
+// IsValidating is true if wt is a validating webhook, else false.
 func (wt *WebhookType) IsValidating() bool {
 	return wt.isValidating_
 }
 
+// IsMutating is true if wt is a mutating webhook, else false.
 func (wt *WebhookType) IsMutating() bool {
 	return !wt.IsValidating()
 }
@@ -161,6 +163,7 @@ func (wt *WebhookType) String() string {
 	return "mutating"
 }
 
+// Equal is true if wt is equivalent to other.
 func (wt *WebhookType) Equal(other *WebhookType) bool {
 	return wt.IsValidating() == other.IsValidating()
 }

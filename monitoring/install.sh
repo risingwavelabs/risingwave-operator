8785c9165cf29b68bd49c2adbd00baaa20e653c1
CAJan93 <jan.mensch@gmx.net>
2022-10-10 18:25:31 +0200

    cli for install script

--- monitoring/install.sh
+++ monitoring/install.sh
@@ -15,6 +15,58 @@
 # limitations under the License.
 #
 
+
+usage() {
+    {
+        echo "This script installs the monitoring stack"
+        echo ""
+        echo "Usage:"
+        echo "$0 [-r] [-k <aws_access_key>] [-s <aws_secret_key>]"
+        echo ""
+        echo "-r    Enable prometheus remote write (AWS). Requires that -k and -s are set"
+        echo "-k    AWS access key"
+        echo "-s    AWS secret key"
+    } 1>&2
+
+    exit 1
+}
+
+
+r=false
+
+while getopts ":k:s:r" o; do
+    case "${o}" in
+        k)
+            k=${OPTARG}
+            ;;
+        s)
+            s=${OPTARG}
+            ;;
+        r)
+            r=true
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+echo "k = ${k}"
+echo "s = ${s}"
+echo "r = ${r}"
+
+# We require credentials, if we use prometheus remote write
+if [[ $r = true ]]; then
+    if [ -z "${s}" ] || [ -z "${k}" ]; then
+        usage
+    fi
+fi
+
+# TODO use k and s if r is set
+# pass to kube-prometheus-stack/install.sh
+exit 1
+
 _SCRIPT_BASEDIR=$(dirname "$0")
 
 cd "${_SCRIPT_BASEDIR}" || exit
@@ -25,10 +77,11 @@ helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
 # Install the `kube-prometheus-stack`
-./kube-prometheus-stack/install.sh
+./kube-prometheus-stack/install.sh # pass parameters here
 
 # Install the `loki-distributed`
 ./loki-distributed/install.sh
 
 # Install the `promtail`
+
 ./promtail/install.sh

d737ecdb4c0ca228d0b9e989c3dbcd43df355d34
ArkBriar <arkbriar@users.noreply.github.com>
2022-12-15 16:53:16 +0800

    ci: rewrite the e2e test (#287)
    * ci: rewrite the e2e test
    
    Signed-off-by: arkbriar <arkbriar@gmail.com>
    
    * ci: add E2E tests for RisingWaveScaleViews
    
    Signed-off-by: arkbriar <arkbriar@gmail.com>
    
    * fix: fix the e2e scripts
    
    Signed-off-by: arkbriar <arkbriar@gmail.com>
    
    Signed-off-by: arkbriar <arkbriar@gmail.com>

---
+++ test/e2e/common/k8s.sh
@@ -0,0 +1,391 @@
+# Copyright 2022 Singularity Data
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+${__E2E_SOURCE_COMMON_K8S_SH__:=false} && return 0 || __E2E_SOURCE_COMMON_K8S_SH__=true
+
+source "$(dirname "${BASH_SOURCE[0]}")/shell.sh"
+source "$(dirname "${BASH_SOURCE[0]}")/logging.sh"
+
+#######################################
+# Utility function for running the kubectl command.
+# Globals
+#   KUBECTL_NAMESPACE
+# Arguments
+#   Arguments for running kubectl.
+# Returns
+#   Code that kubectl returns.
+#######################################
+function k8s::kubectl() {
+  local extra_args=()
+  [[ -v "KUBECTL_NAMESPACE" && -n "${KUBECTL_NAMESPACE}" ]] && extra_args+=(-n "${KUBECTL_NAMESPACE}")
+  kubectl "${extra_args[@]}" "$@"
+}
+
+#######################################
+# Utility function for running the kubectl get command on a specified object. This wrapper hides the output
+# from STDERR when kubectl fails.
+# Globals
+#   KUBECTL_NAMESPACE
+# Arguments
+#   Resource kind, e.g., pod
+#   Resource name, e.g., web-pod-13je7
+#   Other kubectl arguments.
+# Output
+#   STDOUT when succeeds.
+# Returns
+#   0 if the object exists, 255 if not, error code returned by kubectl otherwise.
+#   254 will be returned when the original exit code is 255 to avoid conflict.
+#######################################
+function k8s::kubectl::get() {
+  (($# >= 2)) || { echo >&2 "not enough arguments" && return 1; }
+  [[ -n "$1" ]] || { echo >&2 "resource kind must be provided" && return 1; }
+  [[ -n "$2" ]] || { echo >&2 "resource name must be provided" && return 1; }
+
+  if shell::run_and_capture_outputs k8s::kubectl get "$@"; then
+    echo "${CAPTURED_STDOUT}"
+    return 0
+  else
+    [[ "${CAPTURED_STDERR}" == *"not found"* ]] && return 255
+    ((CAPTURED_EXIT_CODE == 255)) && return 254
+    return "${CAPTURED_EXIT_CODE}"
+  fi
+}
+
+#######################################
+# Utility function for checking if the object exists.
+# Globals
+#   KUBECTL_NAMESPACE
+# Arguments
+#   Resource kind, e.g., pod
+#   Resource name, e.g., web-pod-13je7.
+# Returns
+#   0 if the object exists, 255 if not, error code returned by kubectl otherwise.
+#######################################
+function k8s::kubectl::object_exists() {
+  k8s::kubectl::get "$1" "$2" >/dev/null
+}
+
+#######################################
+# Utility function for checking if a Job is completed.
+# Globals
+#   KUBECTL_NAMESPACE
+# Arguments
+#   Job name.
+# Returns
+#   0 if it is, 1 if not, 255 if the Job object doesn't exists, and other codes when kubectl fails.
+#######################################
+function k8s::job::is_completed() {
+  local complete_status
+  local exit_code=0
+  complete_status=$(k8s::kubectl::get job "$1" -o jsonpath='{.status.conditions[?(@.type=="Complete")].status}') || exit_code=$?
+
+  ((exit_code == 0)) || return "${exit_code}"
+
+  [[ "${complete_status,,}" == "true" ]] || return 1
+}
+
+#######################################
+# Utility function for checking if a Job is failed.
+# Globals
+#   KUBECTL_NAMESPACE
+# Arguments
+#   Job name.
+# Returns
+#   0 if it is, 1 if not, 255 if the Job object doesn't exists, and other codes when kubectl fails.
+#######################################
+function k8s::job::is_failed() {
+  local failed_status
+  local exit_code=0
+
+  failed_status=$(k8s::kubectl::get job "$1" -o jsonpath='{.status.conditions[?(@.type=="Failed")].status}') || exit_code=$?
+
+  ((exit_code == 0)) || return "${exit_code}"
+
+  [[ "${failed_status,,}" == "true" ]] || return 1
+}
+
+K8S_JOB_COMPLETED=""
+K8S_JOB_FAILED=""
+
+#######################################
+# Utility function for checking if a Job is completed or failed.
+# Globals
+#   KUBECTL_NAMESPACE
+#   K8S_JOB_COMPLETED
+#   K8S_JOB_FAILED
+# Arguments
+#   Job name.
+# Returns
+#   0 if it is, 1 if not, 255 if the object doesn't exists, and other codes when kubectl fails.
+#######################################
+function k8s::job::is_completed_or_failed() {
+  local complete_and_failed_status
+  local exit_code=0
+
+  complete_and_failed_status=$(k8s::kubectl::get job "$1" -o jsonpath='{.status.conditions[?(@.type=="Complete")].status},{.status.conditions[?(@.type=="Failed")].status},') || exit_code=$?
+
+  ((exit_code == 0)) || return "${exit_code}"
+
+  # Load output into the array.
+  local -a array
+  IFS="," read -r -a array <<<"${complete_and_failed_status}"
+
+  local complete_status="${array[0]}"
+  local failed_status="${array[1]}"
+
+  # Set the global vars.
+  K8S_JOB_COMPLETED=false
+  K8S_JOB_FAILED=false
+
+  if [[ "${complete_status,,}" == "true" ]]; then
+    K8S_JOB_COMPLETED=true
+  fi
+
+  if [[ "${failed_status,,}" == "true" ]]; then
+    K8S_JOB_FAILED=true
+  fi
+
+  [[ "${K8S_JOB_COMPLETED}" == "true" || "${K8S_JOB_FAILED}" == "true" ]] || return 1
+}
+
+#######################################
+# Utility function for waiting until a Job complete or fail.
+# Globals
+#   KUBECTL_NAMESPACE
+#   KUBECTL_JOB_WAIT_RETRY_LIMIT
+#   KUBECTL_JOB_WAIT_RETRY_INTERVAL
+#   K8S_JOB_COMPLETED
+#   K8S_JOB_FAILED
+# Arguments
+#   Job name.
+# Returns
+#   0 if it completes or fails, 255 if the object doesn't exist, 1 when timeout, and other codes when kubectl fails.
+#######################################
+function k8s::job::wait_before_completed_or_failed() {
+  (($# == 1)) || { echo >&2 "not enough arguments" && return 1; }
+
+  local retry_count=0
+  local retry_limit=${KUBECTL_JOB_WAIT_RETRY_LIMIT:=60}
+  local retry_interval=${KUBECTL_JOB_WAIT_RETRY_INTERVAL:=5}
+
+  local exit_code=0
+  while ((retry_count < retry_limit)); do
+    ((retry_count != 0)) && sleep "${retry_interval}"
+
+    k8s::job::is_completed_or_failed "$1"
+    exit_code=$?
+
+    # Condition met, return.
+    ((exit_code == 0)) && return 0
+
+    # Condition unmet, retry.
+    if ((exit_code == 1)); then
+      retry_count=$((retry_count + 1))
+      continue
+    fi
+
+    # On other errors, just return the exit code.
+    return "${exit_code}"
+  done
+
+  logging::debug "Timeout waiting for Job $1 to complete or fail!"
+  return 1
+}
+
+#######################################
+# Utility function for getting debug info for a Job.
+# Globals
+#   KUBECTL_NAMESPACE
+# Arguments
+#   Job name.
+# Outputs
+#   STDOUT
+# Returns
+#   0 on success, 255 if the Job object doesn't exists, and other codes when kubectl fails.
+#######################################
+function k8s::job::debug() {
+  local job=$1
+
+  local manifest
+  manifest=$(k8s::kubectl::get job "${job}" -o yaml)
+
+  printf "Job manifest in YAML:\n%s\n" "${manifest}"
+  printf "Pods controlled by Job %s:\n%s\n" "${job}" "$(k8s::kubectl::get pod -l job-name="${job}")"
+}
+
+#######################################
+# Utility function for checking if the specified RisingWave is rolled out.
+# Globals
+#   KUBECTL_NAMESPACE
+# Arguments
+#   RisingWave name
+# Returns
+#   0 if it is, 1 if not, 255 if the object doesn't exists, and other codes when kubectl fails.
+#######################################
+function k8s::risingwave::is_rolled_out() {
+  local content
+  content=$(k8s::kubectl::get risingwave "$1" -o jsonpath='{.metadata.generation},{.status.observedGeneration},{.status.conditions[?(@.type=="Running")].status},{.status.conditions[?(@.type=="Upgrading")].status},') || return $?
+
+  # Load output into the array.
+  local -a generation_and_conditions
+  IFS="," read -r -a generation_and_conditions <<<"${content}"
+
+  local current_generation="${generation_and_conditions[0]}"
+  local observed_generation="${generation_and_conditions[1]}"
+  local running_condition="${generation_and_conditions[2]}"
+  local upgrading_condition="${generation_and_conditions[3]}"
+
+  if ((current_generation == observed_generation)) &&
+    [[ "${running_condition}" == "True" && ("${upgrading_condition}" == "" || "${upgrading_condition}" == "False") ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+#######################################
+# Utility function for waiting before the specified RisingWave is rolled out.
+# Globals
+#   KUBECTL_NAMESPACE
+#   KUBECTL_RISINGWAVE_WAIT_RETRY_LIMIT
+#   KUBECTL_RISINGWAVE_WAIT_RETRY_INTERVAL
+# Arguments
+#   RisingWave name
+# Returns
+#   0 if it is, 1 if not, 255 if the object doesn't exists, and other codes when kubectl fails.
+#######################################
+function k8s::risingwave::wait_before_rollout() {
+  (($# == 1)) || { echo >&2 "not enough arguments" && return 1; }
+
+  local retry_count=0
+  local retry_limit=${KUBECTL_RISINGWAVE_WAIT_RETRY_LIMIT:=60}
+  local retry_interval=${KUBECTL_RISINGWAVE_WAIT_RETRY_INTERVAL:=5}
+
+  local exit_code=0
+  while ((retry_count < retry_limit)); do
+    ((retry_count != 0)) && sleep "${retry_interval}"
+
+    k8s::risingwave::is_rolled_out "$1"
+    exit_code=$?
+
+    # Condition met, return.
+    ((exit_code == 0)) && return 0
+
+    # Condition unmet, retry.
+    if ((exit_code == 1)); then
+      retry_count=$((retry_count + 1))
+      continue
+    fi
+
+    # On other errors, just return the exit code.
+    return "${exit_code}"
+  done
+
+  logging::debug "Timeout waiting for RisingWave $1 to rollout!"
+  return 1
+}
+
+#######################################
+# Utility function for getting debug info for a RisingWave.
+# Globals
+#   KUBECTL_NAMESPACE
+# Arguments
+#   RisingWave name.
+# Outputs
+#   STDOUT
+# Returns
+#   0 on success, 255 if the RisingWave object doesn't exists, and other codes when kubectl fails.
+#######################################
+function k8s::risingwave::debug() {
+  local risingwave=$1
+
+  local manifest
+  manifest=$(k8s::kubectl::get risingwave "${risingwave}" -o yaml)
+
+  printf "RisingWave manifest in YAML:\n%s\n" "${manifest}"
+  printf "Pods controlled by RisingWave %s:\n%s\n" "${risingwave}" "$(k8s::kubectl::get pod -l risingwave/name="${risingwave}")"
+}
+
+#######################################
+# Check if the deployment has been rolled out.
+# Globals
+#   KUBECTL_NAMESPACE
+# Arguments
+#   Deployment name.
+# Returns
+#   0 for true, non-zero when false or error occurs.
+#######################################
+function k8s::deployment::is_rolled_out() {
+  local content
+  content=$(k8s::kubectl::get deployment "$1" -o jsonpath='{.metadata.generation},{.status.observedGeneration},{.spec.replicas},{.status.availableReplicas},{.status.readyReplicas},{.status.replicas},{.status.updatedReplicas},') || return $?
+
+  # Load output into the array.
+  local -a array
+  IFS="," read -r -a array <<<"${content}"
+
+  local generation="${array[0]}"
+  local observed_generation="${array[1]}"
+  local replicas="${array[2]}"
+  local available_replicas="${array[3]}"
+  # local ready_replicas="${array[4]}"
+  local current_replicas="${array[5]}"
+  local updated_replicas="${array[6]}"
+
+  ((generation == observed_generation)) || return 1
+  ((updated_replicas >= replicas)) || return 1
+  ((current_replicas <= current_replicas)) || return 1
+  ((available_replicas >= updated_replicas)) || return 1
+}
+
+#######################################
+# Utility function for waiting before the specified Deployment is rolled out.
+# Globals
+#   KUBECTL_NAMESPACE
+#   KUBECTL_WAIT_RETRY_LIMIT
+#   KUBECTL_WAIT_RETRY_INTERVAL
+# Arguments
+#   Deployment name
+# Returns
+#   0 if it is, 1 if not, 255 if the object doesn't exists, and other codes when kubectl fails.
+#######################################
+function k8s::deployment::wait_before_rollout() {
+  (($# == 1)) || { echo >&2 "not enough arguments" && return 1; }
+
+  local retry_count=0
+  local retry_limit=${KUBECTL_WAIT_RETRY_LIMIT:=60}
+  local retry_interval=${KUBECTL_WAIT_RETRY_INTERVAL:=5}
+
+  local exit_code=0
+  while ((retry_count < retry_limit)); do
+    ((retry_count != 0)) && sleep "${retry_interval}"
+
+    k8s::deployment::is_rolled_out "$1"
+    exit_code=$?
+
+    # Condition met, return.
+    ((exit_code == 0)) && return 0
+
+    # Condition unmet, retry.
+    if ((exit_code == 1)); then
+      retry_count=$((retry_count + 1))
+      continue
+    fi
+
+    # On other errors, just return the exit code.
+    return "${exit_code}"
+  done
+
+  logging::debug "Timeout waiting for Deployment $1 to rollout!"
+  return 1
+}
---
+++ test/e2e/common/logging.sh
@@ -0,0 +1,436 @@
+# Copyright 2022 Singularity Data
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+${__E2E_SOURCE_COMMON_LOGGING_SH__:=false} && return 0 || __E2E_SOURCE_COMMON_LOGGING_SH__=true
+
+source "$(dirname "${BASH_SOURCE[0]}")/shell.sh"
+
+# Bash version check: must be >= 4 to has associated array supported!
+shell::assert_minimum_bash_version 4 4
+
+# Associate array for color name and ansi code mappings. (Readonly)
+readonly -A _LOGGING_ANSI_COLORS=(
+  ["red"]="\033[00;31m"
+  ["green"]="\033[00;32m"
+  ["yellow"]="\033[00;33m"
+  ["blue"]="\033[00;34m"
+  ["magenta"]="\033[00;35m"
+  ["purple"]="\033[00;36m"
+  ["light-gray"]="\033[00;37m"
+  ["light-red"]="\033[01;31m"
+  ["light-green"]="\033[01;32m"
+  ["light-yellow"]="\033[01;33m"
+  ["light-blue"]="\033[01;34m"
+  ["light-magenta"]="\033[01;35m"
+  ["light-purple"]="\033[01;36m"
+  ["white"]="\033[01;37m"
+  ["escape"]="\033[0m"
+)
+
+#######################################
+# Utility function for wrapping texts with ansi color codes.
+# Globals
+#   _LOGGING_ANSI_COLORS
+# Arguments
+#   Color name, e.g., red
+#   Variable length strings
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function color::ansi() {
+  echo -en "${_LOGGING_ANSI_COLORS[$1]}"
+  echo -en "${@:2}"
+  echo -en "${_LOGGING_ANSI_COLORS[escape]}"
+}
+
+#######################################
+# Utility function for wrapping texts with green codes.
+# Globals
+#   _LOGGING_ANSI_COLORS
+# Arguments
+#   Variable length strings
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function color::ansi::green() {
+  color::ansi green "$@"
+}
+
+#######################################
+# Utility function for wrapping texts with red codes.
+# Globals
+#   _LOGGING_ANSI_COLORS
+# Arguments
+#   Variable length strings
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function color::ansi::red() {
+  color::ansi red "$@"
+}
+
+#######################################
+# Utility function for wrapping texts with blue codes.
+# Globals
+#   _LOGGING_ANSI_COLORS
+# Arguments
+#   Variable length strings
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function color::ansi::blue() {
+  color::ansi blue "$@"
+}
+
+#######################################
+# Utility function for wrapping texts with yellow codes.
+# Globals
+#   _LOGGING_ANSI_COLORS
+# Arguments
+#   Variable length strings
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function color::ansi::yellow() {
+  color::ansi yellow "$@"
+}
+
+#######################################
+# Utility function for wrapping texts with magenta codes.
+# Globals
+#   _LOGGING_ANSI_COLORS
+# Arguments
+#   Variable length strings
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function color::ansi::magenta() {
+  color::ansi magenta "$@"
+}
+
+#######################################
+# Utility function for wrapping texts with purple codes.
+# Globals
+#   _LOGGING_ANSI_COLORS
+# Arguments
+#   Variable length strings
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function color::ansi::purple() {
+  color::ansi purple "$@"
+}
+
+# Constant integer values of log levels. (Readonly)
+readonly _LOGGING_LEVEL_DISABLED=0
+readonly _LOGGING_LEVEL_ERROR=1
+readonly _LOGGING_LEVEL_WARN=2
+readonly _LOGGING_LEVEL_INFO=3
+readonly _LOGGING_LEVEL_DEBUG=4
+
+# Associate array for log levels. (Readonly)
+readonly -A _LOGGING_LEVELS=(
+  ["off"]=${_LOGGING_LEVEL_DISABLED}
+  ["error"]=${_LOGGING_LEVEL_ERROR}
+  ["warn"]=${_LOGGING_LEVEL_WARN}
+  ["info"]=${_LOGGING_LEVEL_INFO}
+  ["debug"]=${_LOGGING_LEVEL_DEBUG}
+)
+
+# Variables to control the logging behaviors.
+_LOGGING_LEVEL=_LOGGING_LEVEL_INFO
+_LOGGING_ERROR_TO_STDERR=true
+
+#######################################
+# Utility function for getting the log level in integer.
+# Globals
+#   _LOGGING_LEVEL
+# Arguments
+#   Log level in either string or index.
+# Outputs
+#   STDOUT
+#   STDERR
+# Return
+#   0 if the log level is found, non-zero otherwise.
+#######################################
+function logging::_level_int_val() {
+  if [[ "$1" =~ ^[0-9]+$ ]]; then
+    # Is a number
+    echo "$1"
+  else
+    # Is a string, use ${var,,} to convert the string to lowercase.
+    local level=${1,,}
+    if [[ -v "_LOGGING_LEVELS[${level}]" ]]; then
+      local val=${_LOGGING_LEVELS[${level}]}
+      echo "${val}"
+    else
+      echo >&2 "unknown log level: ${1}"
+      return 1
+    fi
+  fi
+}
+
+#######################################
+# Utility function for checking if the log level is enabled.
+# Globals
+#   _LOGGING_LEVEL
+# Arguments
+#   Target log level in integer.
+# Return
+#   0 if true, non-zero otherwise.
+#######################################
+function logging::_is_level_enabled() {
+  ((_LOGGING_LEVEL >= $1))
+}
+
+#######################################
+# Utility function for setting the log level.
+# Globals
+#   _LOGGING_LEVEL
+# Arguments
+#   Log level in either string or index.
+# Return
+#   0 if the log level is found and set, non-zero otherwise.
+#######################################
+function logging::set_level() {
+  (($# == 1)) || { echo >&2 "not enough arguments" && return 1; }
+  [[ -n "$1" ]] || { echo >&2 "level must be provided" && return 1; }
+
+  _LOGGING_LEVEL=$(logging::_level_int_val "$1")
+}
+
+#######################################
+# Utility function for enabling the logging. By default, the log level is set to INFO.
+# Globals
+#   _LOGGING_LEVEL
+# Arguments
+#   None
+# Return
+#   0
+#######################################
+function logging::enable() {
+  logging::set_level info
+}
+
+#######################################
+# Utility function for disabling the logging.
+# Globals
+#   _LOGGING_LEVEL
+# Arguments
+#   None
+# Return
+#   0
+#######################################
+function logging::disable() {
+  logging::set_level off
+}
+
+#######################################
+# Utility function for getting the additional tags.
+# Globals
+#   LOGGING_TAGS
+# Arguments
+#   None
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function logging::_additional_tags() {
+  if [[ -v "LOGGING_TAGS" ]]; then
+    # If LOGGING_TAGS isn't an array, convert it to an array.
+    if [[ ! "$(declare -p LOGGING_TAGS)" =~ "declare -a" ]]; then
+      # shellcheck disable=SC2206
+      LOGGING_TAGS=(${LOGGING_TAGS})
+    fi
+
+    local quote_tags
+    quote_tags=("${LOGGING_TAGS[@]/#/[}")
+    echo " ${quote_tags[*]/%/]}"
+  fi
+}
+
+#######################################
+# Utility function for printing messages in log level INFO.
+# Globals
+#   _LOGGING_LEVEL
+# Arguments
+#   Variable length strings.
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function logging::info() {
+  if logging::_is_level_enabled _LOGGING_LEVEL_INFO; then
+    echo "$(color::ansi::blue "[INFO]")$(logging::_additional_tags)" "$@"
+  fi
+}
+
+#######################################
+# Utility function for formatting and printing messages in log level INFO.
+# Globals
+#   _LOGGING_LEVEL
+#   LOGGING_TAGS
+# Arguments
+#   Format string.
+#   Variable length strings.
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function logging::infof() {
+  # shellcheck disable=SC2059
+  if logging::_is_level_enabled _LOGGING_LEVEL_INFO; then
+    printf "$(color::ansi::blue "[INFO]")$(logging::_additional_tags) $1" "${@:2}"
+  fi
+}
+
+#######################################
+# Utility function for printing messages in log level WARN.
+# Globals
+#   _LOGGING_LEVEL
+#   LOGGING_TAGS
+# Arguments
+#   Variable length strings.
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function logging::warn() {
+  if logging::_is_level_enabled _LOGGING_LEVEL_WARN; then
+    echo "$(color::ansi::yellow "[WARN]")$(logging::_additional_tags)" "$@"
+  fi
+}
+
+#######################################
+# Utility function for formatting and printing messages in log level WARN.
+# Globals
+#   _LOGGING_LEVEL
+#   LOGGING_TAGS
+# Arguments
+#   Format string.
+#   Variable length strings.
+# Outputs
+#   STDOUT
+# Return
+#   0
+#######################################
+function logging::warnf() {
+  # shellcheck disable=SC2059
+  if logging::_is_level_enabled _LOGGING_LEVEL_WARN; then
+    printf "$(color::ansi::yellow "[WARN]")$(logging::_additional_tags) $1" "${@:2}"
+  fi
+}
+
+#######################################
+# Utility function for printing messages in log level ERROR.
+# Globals
+#   _LOGGING_LEVEL
+#   _LOGGING_ERROR_TO_STDERR, decides if the output is streamed to stderr.
+#   LOGGING_TAGS
+# Arguments
+#   Variable length strings.
+# Outputs
+#   STDERR if _LOGGING_ERROR_TO_STDERR is true (by default), STDOUT otherwise.
+# Return
+#   0
+#######################################
+function logging::error() {
+  if logging::_is_level_enabled _LOGGING_LEVEL_ERROR; then
+    if [[ "${_LOGGING_ERROR_TO_STDERR}" == true ]]; then
+      echo >&2 "$(color::ansi::red "[ERRO]")$(logging::_additional_tags)" "$@"
+    else
+      echo "$(color::ansi::red "[ERRO]")$(logging::_additional_tags)" "$@"
+    fi
+  fi
+}
+
+#######################################
+# Utility function for formatting and printing messages in log level ERROR.
+# Globals
+#   _LOGGING_LEVEL
+#   LOGGING_TAGS
+# Arguments
+#   Format string.
+#   _LOGGING_ERROR_TO_STDERR, decides if the output is streamed to stderr.
+# Outputs
+#   STDERR if _LOGGING_ERROR_TO_STDERR is true (by default), STDOUT otherwise.
+# Return
+#   0
+#######################################
+function logging::errorf() {
+  # shellcheck disable=SC2059
+  if logging::_is_level_enabled _LOGGING_LEVEL_ERROR; then
+    if [[ "${_LOGGING_ERROR_TO_STDERR}" == true ]]; then
+      printf >&2 "$(color::ansi::red "[ERRO]")$(logging::_additional_tags) $1" "${@:2}"
+    else
+      printf "$(color::ansi::red "[ERRO]")$(logging::_additional_tags) $1" "${@:2}"
+    fi
+  fi
+}
+
+#######################################
+# Utility function for printing messages in log level DEBUG. The output is always streamed to stderr.
+# Globals
+#   _LOGGING_LEVEL
+#   LOGGING_TAGS
+# Arguments
+#   Variable length strings.
+# Outputs
+#   STDERR
+# Return
+#   0
+#######################################
+function logging::debug() {
+  if logging::_is_level_enabled _LOGGING_LEVEL_DEBUG; then
+    echo >&2 "[DEBU]$(logging::_additional_tags)" "$@"
+  fi
+}
+
+#######################################
+# Utility function for formatting and printing messages in log level DEBUG. The output is always streamed to stderr.
+# Globals
+#   _LOGGING_LEVEL
+#   LOGGING_TAGS
+# Arguments
+#   Format string.
+#   Variable length strings.
+# Outputs
+#   STDERR
+# Return
+#   0
+#######################################
+function logging::debugf() {
+  # shellcheck disable=SC2059
+  if logging::_is_level_enabled _LOGGING_LEVEL_DEBUG; then
+    printf >&2 "[DEBU]$(logging::_additional_tags) $1" "${@:2}"
+  fi
+}

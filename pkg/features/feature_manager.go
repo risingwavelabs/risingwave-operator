6bd720fe564bd35bee47764adc4a5c4ef7d45afa
sixletters <62091272+sixletters@users.noreply.github.com>
2022-12-21 14:58:29 +0800

    feat: support feature gates (#289)
    * feature gate utility
    
    * feature manager implementation
    
    * test-cases for feature manager
    
    * additional test cases for feature manager parsing
    
    * Update feature_manager_test.go

---
+++ pkg/features/feature_manager.go
@@ -0,0 +1,259 @@
+/*
+ * Copyright 2022 Singularity Data
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package features
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+type FeatureStage string
+type FeatureName string
+
+const (
+	EnableOpenKruiseFeature FeatureName = "EnableOpenKruise"
+)
+
+const (
+	Alpha FeatureStage = "Alpha"
+	Beta  FeatureStage = "Beta"
+)
+
+var risingWaveFeatureManager *FeatureManager
+
+var (
+	SupportedFeatureList = []Feature{
+		{
+			Name:          EnableOpenKruiseFeature,
+			Description:   "This feature enables open kruise as an optional provider",
+			DefaultEnable: false,
+			Stage:         Beta,
+		},
+	}
+)
+
+type Feature struct {
+	Name          FeatureName
+	Description   string
+	Enabled       bool
+	DefaultEnable bool
+	Stage         FeatureStage
+}
+
+// This method return a pointer to a deep copy of a feature struct.
+func (f *Feature) DeepCopy() *Feature {
+	return &Feature{
+		Name:          f.Name,
+		Description:   f.Description,
+		Enabled:       f.Enabled,
+		DefaultEnable: f.DefaultEnable,
+		Stage:         f.Stage,
+	}
+}
+
+// This method returns a pointer to an instance of a Feature struct.
+func newFeature(name FeatureName, desc string, defaultEnable bool, stage FeatureStage) *Feature {
+	return &Feature{
+		Name:          name,
+		Description:   desc,
+		DefaultEnable: defaultEnable,
+		Stage:         stage,
+	}
+}
+
+type FeatureManager struct {
+	// Feature map is an internal structure that stores a feature name and a pointer to a feature struct.
+	featureMap map[FeatureName]*Feature
+}
+
+// Helper function that returns a pointer to an instance of the FeatureManager.
+func NewRisingWaveFeatureManager() *FeatureManager {
+	return &FeatureManager{
+		featureMap: make(map[FeatureName]*Feature),
+	}
+}
+
+// This functions initializes the FeatureManager with the current supported Features.
+func InitFeatureManagerWithSupportedFeatures(supportedFeatureList []Feature) *FeatureManager {
+	risingWaveFeatureManager = NewRisingWaveFeatureManager()
+	for _, supportedFeature := range supportedFeatureList {
+		supportedFeature.Enabled = supportedFeature.DefaultEnable
+		risingWaveFeatureManager.addFeature(&supportedFeature)
+	}
+	return risingWaveFeatureManager
+}
+
+// This functions initializes the FeatureManager with the current supported Features and also parses the feature gate string.
+func InitFeatureManager(supportedFeatureList []Feature, featureGateString string) *FeatureManager {
+	risingWaveFeatureManager = NewRisingWaveFeatureManager()
+	for _, supportedFeature := range supportedFeatureList {
+		supportedFeature.Enabled = supportedFeature.DefaultEnable
+		risingWaveFeatureManager.addFeature(&supportedFeature)
+	}
+	if risingWaveFeatureManager.ParseFromFeatureGateString(featureGateString) != nil {
+		panic("Invalid value given to feature-gates argument")
+	}
+	return risingWaveFeatureManager
+}
+
+// This method returns the Feature Manager Struct. Should not be modified after intialization.
+func GetFeatureManager() *FeatureManager {
+	return risingWaveFeatureManager
+}
+
+// This is a helper functions that adds a feature to the featureManager, will be used on init to init and add all
+// features to the featureManager.
+func (m *FeatureManager) addFeature(feature *Feature) {
+	m.featureMap[feature.Name] = feature.DeepCopy()
+}
+
+// This function returns true if the feature exists in the featureManager, else returns false.
+func (m FeatureManager) IsFeatureExist(featureName FeatureName) bool {
+	_, exist := m.featureMap[featureName]
+	return exist
+}
+
+// This is a helper function that helps to set a feature to a given boolean, if feature does not exist,
+// returns an error. Used in EnableFeature and DisableFeature methods. Not visible to users.
+func (m *FeatureManager) setFeatureEnable(name FeatureName, enable bool) error {
+	// check for existence of feature in map
+	_, featureExists := m.featureMap[name]
+	if featureExists {
+		m.featureMap[name].Enabled = enable
+		return nil
+	}
+	return fmt.Errorf("the following feature does not exist: %s", name)
+}
+
+// This method takes in a feature name and checks if it is enabled, returns a bool, nil if it exists,
+// and a false,error if it does not exist in the featureManager.
+func (m FeatureManager) IsFeatureEnabled(name FeatureName) bool {
+	// check for existence of feature in map
+	feature, featureExists := m.featureMap[name]
+	return featureExists && feature.Enabled
+}
+
+// This metod takes in a feature name and enables it if it exists, if it does not
+// it returns an error.
+func (m *FeatureManager) EnableFeature(name FeatureName) error {
+	// check for existence of feature in map
+	return m.setFeatureEnable(name, true)
+}
+
+// This metod takes in a feature name and disables it if it exists, if it does not
+// it returns an error.
+func (m *FeatureManager) DisableFeature(name FeatureName) error {
+	// check for existence of feature in map
+	return m.setFeatureEnable(name, false)
+}
+
+// This method returns the number of features in the featureManager.
+func (m *FeatureManager) GetNumOfFeatures() int {
+	return len(m.featureMap)
+}
+
+// This method lists all features, returns a copy of the list of feature structs.
+func (m FeatureManager) ListFeatures() []Feature {
+	var featureList = []Feature{}
+	for _, feature := range m.featureMap {
+		// make a deep copy of the feature
+		featureList = append(featureList, *feature.DeepCopy())
+	}
+	return featureList
+}
+
+// This method lists all enabled features, returns a copy of the list of feature structs.
+func (m FeatureManager) ListEnabledFeatures() []Feature {
+	var featureList = []Feature{}
+	for _, feature := range m.featureMap {
+		if feature.Enabled {
+			// make a copy of the feature
+			featureList = append(featureList, *feature.DeepCopy())
+		}
+	}
+	return featureList
+}
+
+// This method lists all disabled features, returns a copy of list of feature structs.
+func (m FeatureManager) ListDisabledFeatures() []Feature {
+	var featureList = []Feature{}
+	for _, feature := range m.featureMap {
+		if !feature.Enabled {
+			// make a deep copy of the feature
+			featureList = append(featureList, *feature.DeepCopy())
+		}
+	}
+	return featureList
+}
+
+// This method takes in a feature name and return a copy of the feature struct with all its meta information.
+func (m FeatureManager) GetFeature(name FeatureName) (Feature, error) {
+	_, featureExists := m.featureMap[name]
+	if !featureExists {
+		return Feature{}, fmt.Errorf("the following feature does not exist: %s", name)
+	}
+	// make a deep copy of the feature, every other primitive field is copied implicitly.
+	return *m.featureMap[name].DeepCopy(), nil
+}
+
+// This method takes in a feature gate string that is given as a CLI argument,
+// parses the features and updates the featureManager. e.g if command line argument is
+// --feature-gates=enableOpenKruise=true,otherOption=false, it will set the feature enableOpenKruise
+// as true if and only if it exists. if a feature is not supported, it is simply ignored.
+func (m *FeatureManager) ParseFromFeatureGateString(featureGateString string) error {
+	if len(featureGateString) == 0 {
+		return nil
+	}
+	featureGateString = strings.TrimSpace(featureGateString)
+	if !unicode.IsLetter([]rune(featureGateString[0:1])[0]) {
+		return fmt.Errorf("parsing error of feature gate string: %s", featureGateString)
+	}
+	featureGatesArgs := strings.Split(featureGateString, ",")
+	for _, featureString := range featureGatesArgs {
+		featureName, enabled, err := parseFeatureString(featureString)
+		if err != nil {
+			return err
+		}
+		_, featureExists := m.featureMap[featureName]
+		if !featureExists {
+			continue
+		}
+		m.featureMap[featureName].Enabled = enabled
+	}
+	return nil
+}
+
+// This function parses a feature string into a featurename and a boolean and returns
+// an error when a feature string cannot be parsed. e.g enableOpenKruise=true will return
+// (enableOpenKruise, true, nil).
+func parseFeatureString(featureString string) (FeatureName, bool, error) {
+	featureStringSplit := strings.Split(featureString, "=")
+	if len(featureStringSplit) != 2 {
+		return "", false, fmt.Errorf("invalid feature syntax given: %s", featureString)
+	}
+	featureName := strings.TrimSpace(featureStringSplit[0])
+	if len(featureName) == 0 {
+		return "", false, fmt.Errorf("invalid feature name given: %s", featureName)
+	}
+	enabled, err := strconv.ParseBool(strings.TrimSpace(featureStringSplit[1]))
+	if err != nil {
+		return "", false, fmt.Errorf("invalid feature status: %s, parse failed: %w", featureString, err)
+	}
+	return FeatureName(featureName), enabled, nil
+}
---
+++ pkg/features/feature_manager_test.go
@@ -0,0 +1,429 @@
+/*
+ * Copyright 2022 Singularity Data
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package features
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func newRisingWaveSupportedFeatureListForTest() []Feature {
+	return []Feature{
+		{
+			Name:          "feature-1",
+			Description:   "feature-1 desc",
+			DefaultEnable: false,
+			Enabled:       false,
+			Stage:         Beta,
+		},
+		{
+			Name:          "feature-2",
+			Description:   "feature-2 desc",
+			DefaultEnable: true,
+			Enabled:       true,
+			Stage:         Alpha,
+		},
+		{
+			Name:          "feature-3",
+			Description:   "feature-3 desc",
+			DefaultEnable: true,
+			Enabled:       true,
+			Stage:         Beta,
+		},
+	}
+}
+
+func getNonExistentFeatureName() FeatureName {
+	return FeatureName("feature-4")
+}
+func isFeatureEqual(f1 Feature, f2 Feature) bool {
+	return reflect.DeepEqual(f1, f2)
+}
+
+func newRisingWaveFeatureManagerForTest(featureGates string) *FeatureManager {
+	return InitFeatureManager(newRisingWaveSupportedFeatureListForTest(), featureGates)
+}
+
+func TestIsFeatureEnabled(t *testing.T) {
+	// Get a fake risingwave feature manager that was initialized with the supported FeatureList.
+	fakeRisingWaveFeatureManager := newRisingWaveFeatureManagerForTest("")
+
+	// Test for features in supported list with default true/false value and for features that do not exist.
+	testcases := map[string]struct {
+		featureName FeatureName
+		expected    bool
+	}{
+		"enabled-feature": {
+			featureName: FeatureName("feature-2"),
+			expected:    true,
+		},
+		"disabled-feature": {
+			featureName: FeatureName("feature-1"),
+			expected:    false,
+		},
+		"feature-not-exist": {
+			featureName: getNonExistentFeatureName(),
+			expected:    false,
+		},
+	}
+	for _, tc := range testcases {
+		if fakeRisingWaveFeatureManager.IsFeatureEnabled(tc.featureName) != tc.expected {
+			t.Fatal("Feature Enabled/Disabled wrongly")
+		}
+	}
+}
+
+func TestEnableFeature(t *testing.T) {
+	// Get a fake risingwave feature manager that was initialized with the supported FeatureList.
+	fakeRisingWaveFeatureManager := newRisingWaveFeatureManagerForTest("")
+
+	// feature-1 is by default disabled, so enabling it would suffice disabled -> enabled case.
+	// feature-2 is by default enabled, so enabling it would suffice for enabled -> enabled case.
+	testcases := map[string]struct {
+		featureName FeatureName
+		exist       bool
+		expected    bool
+	}{
+		"enable-enabled-feature": {
+			featureName: FeatureName("feature-2"),
+			exist:       true,
+		},
+		"enable-disabled-feature": {
+			featureName: FeatureName("feature-1"),
+			exist:       true,
+		},
+		"feature-not-exist": {
+			featureName: getNonExistentFeatureName(),
+			exist:       false,
+		},
+	}
+
+	for _, tc := range testcases {
+		err := fakeRisingWaveFeatureManager.EnableFeature(tc.featureName)
+		if tc.exist && err != nil {
+			t.Fatal("Feature exists but enabling it is throwing an error.")
+		}
+		if tc.exist && !fakeRisingWaveFeatureManager.IsFeatureEnabled(tc.featureName) {
+			t.Fatal("Feature was not enabled.")
+		}
+	}
+}
+
+func TestDisableFeature(t *testing.T) {
+	// Get a fake risingwave feature manager that was initialized with the supported FeatureList.
+	fakeRisingWaveFeatureManager := newRisingWaveFeatureManagerForTest("")
+
+	// feature-1 is by default disabled, so enabling it would suffice disabled -> disabled case.
+	// feature-2 is by default enabled, so enabling it would suffice for enabled -> disabled case.
+	testcases := map[string]struct {
+		featureName FeatureName
+		exist       bool
+		expected    bool
+	}{
+		"disable-enabled-feature": {
+			featureName: FeatureName("feature-2"),
+			exist:       true,
+			expected:    true,
+		},
+		"enable-disabled-feature": {
+			featureName: FeatureName("feature-1"),
+			exist:       true,
+			expected:    false,
+		},
+		"feature-not-exist": {
+			featureName: getNonExistentFeatureName(),
+			exist:       false,
+			expected:    false,
+		},
+	}
+
+	for _, tc := range testcases {
+		err := fakeRisingWaveFeatureManager.DisableFeature(tc.featureName)
+		if tc.exist && err != nil {
+			t.Fatal("Feature exists but disabling it is throwing an error.")
+		}
+		if tc.exist && fakeRisingWaveFeatureManager.IsFeatureEnabled(tc.featureName) {
+			t.Fatal("Feature was not disabled.")
+		}
+	}
+
+}
+
+func TestGetNumOfFeatures(t *testing.T) {
+	// Get a fake risingwave feature manager that was initialized with the supported FeatureList.
+	fakeRisingWaveFeatureManager := newRisingWaveFeatureManagerForTest("")
+
+	// Get the actual supported Feature List to test for comparison.
+	supportedFeatureList := newRisingWaveSupportedFeatureListForTest()
+
+	if len(supportedFeatureList) != fakeRisingWaveFeatureManager.GetNumOfFeatures() {
+		t.Fatal("Number of features do not math")
+	}
+}
+
+func TestListFeatures(t *testing.T) {
+	// Get a fake risingwave feature manager that was initialized with the supported FeatureList.
+	fakeRisingWaveFeatureManager := newRisingWaveFeatureManagerForTest("")
+
+	// Get the actual supported Feature List to test for comparison.
+	supportedFeatureList := newRisingWaveSupportedFeatureListForTest()
+
+	// Make a map of the feature name to index of feature in supported feature list for fast lookup.
+	featureListIndexMap := make(map[FeatureName]int)
+	for idx, feature := range supportedFeatureList {
+		featureListIndexMap[feature.Name] = idx
+	}
+
+	// Test for comparison between features
+	for _, feature := range fakeRisingWaveFeatureManager.ListFeatures() {
+		actualIdx := featureListIndexMap[feature.Name]
+		if !isFeatureEqual(feature, supportedFeatureList[actualIdx]) {
+			t.Fatal("All features in feature list do not match")
+		}
+	}
+}
+
+func TestListEnabledFeatures(t *testing.T) {
+	// Get a fake risingwave feature manager that was initialized with the supported FeatureList.
+	fakeRisingWaveFeatureManager := newRisingWaveFeatureManagerForTest("")
+
+	// Get the actual supported Feature List to test for comparison.
+	supportedFeatureList := newRisingWaveSupportedFeatureListForTest()
+
+	// Make a map of the feature name to index of feature in supported feature list for fast lookup.
+	featureListIndexMap := make(map[FeatureName]int)
+	for idx, feature := range supportedFeatureList {
+		featureListIndexMap[feature.Name] = idx
+	}
+
+	// We test if all features are enabled and also check the previous state in the supportedFeatureList,
+	// to check if it was also enabled.
+	for _, feature := range fakeRisingWaveFeatureManager.ListEnabledFeatures() {
+		if !fakeRisingWaveFeatureManager.IsFeatureEnabled(feature.Name) {
+			t.Fatal("Disabled feature present in enabled feature list.")
+		}
+		if !supportedFeatureList[featureListIndexMap[feature.Name]].Enabled {
+			t.Fatal("Feature was previously not enabled.")
+		}
+	}
+}
+
+func TestListDisabledFeatures(t *testing.T) {
+	// Get a fake risingwave feature manager that was initialized with the supported FeatureList.
+	fakeRisingWaveFeatureManager := newRisingWaveFeatureManagerForTest("")
+
+	// Get the actual supported Feature List to test for comparison.
+	supportedFeatureList := newRisingWaveSupportedFeatureListForTest()
+
+	// Make a map of the feature name to index of feature in supported feature list for fast lookup.
+	featureListIndexMap := make(map[FeatureName]int)
+	for idx, feature := range supportedFeatureList {
+		featureListIndexMap[feature.Name] = idx
+	}
+
+	// We test if all features are disabled and also check the previous state in the supportedFeatureList,
+	// to check if it was also dsiabled.
+	for _, feature := range fakeRisingWaveFeatureManager.ListDisabledFeatures() {
+		if fakeRisingWaveFeatureManager.IsFeatureEnabled(feature.Name) {
+			t.Fatal("Enabled feature present in disabled feature list.")
+		}
+		if supportedFeatureList[featureListIndexMap[feature.Name]].Enabled {
+			t.Fatal("Feature was previously not disabled.")
+		}
+	}
+
+}
+
+func TestGetFeature(t *testing.T) {
+	// Get a fake risingwave feature manager that was initialized with the supported FeatureList.
+	fakeRisingWaveFeatureManager := newRisingWaveFeatureManagerForTest("")
+
+	// Get the actual supported Feature List to test for comparison.
+	supportedFeatureList := newRisingWaveSupportedFeatureListForTest()
+
+	// Make a map of the feature name to index of feature in supported feature list for fast lookup.
+	featureListIndexMap := make(map[FeatureName]int)
+	for idx, feature := range supportedFeatureList {
+		featureListIndexMap[feature.Name] = idx
+	}
+
+	for _, supportedFeature := range supportedFeatureList {
+		feature, err := fakeRisingWaveFeatureManager.GetFeature(supportedFeature.Name)
+		if err != nil {
+			t.Fatal("There was an error in retrieving a supported feature.")
+		}
+		if !isFeatureEqual(feature, supportedFeature) {
+			t.Fatal("Retrieved feature is not equal to expected feature.")
+		}
+	}
+
+	// We have to test for if a fature does not exist as well.
+	_, err := fakeRisingWaveFeatureManager.GetFeature(getNonExistentFeatureName())
+	if err == nil {
+		t.Fatal("Error not thrown for non existent feature request.")
+	}
+
+}
+
+func TestParseFeatureString(t *testing.T) {
+	testcases := map[string]struct {
+		featureString   string
+		expectedName    FeatureName
+		expectedEnabled bool
+		isError         bool
+	}{
+		"Valid-feature-string-enable-true": {
+			featureString:   "openKruise=true",
+			expectedName:    FeatureName("openKruise"),
+			expectedEnabled: true,
+			isError:         false,
+		},
+		"Valid-feature-string-disable-false": {
+			featureString:   "openKruise=false",
+			expectedName:    FeatureName("openKruise"),
+			expectedEnabled: false,
+			isError:         false,
+		},
+		"Valid-feature-string-enable-1": {
+			featureString:   "openKruise=1",
+			expectedName:    FeatureName("openKruise"),
+			expectedEnabled: true,
+			isError:         false,
+		},
+		"Valid-feature-string-enable-0": {
+			featureString:   "openKruise=0",
+			expectedName:    FeatureName("openKruise"),
+			expectedEnabled: false,
+			isError:         false,
+		},
+		"Invalid-feature-string-with-2-equals": {
+			featureString:   "openKruise=true=",
+			expectedName:    FeatureName(""),
+			expectedEnabled: false,
+			isError:         true,
+		},
+		"Invalid-feature-string-with-multiple-equals": {
+			featureString:   "==openKruise=true=",
+			expectedName:    FeatureName(""),
+			expectedEnabled: false,
+			isError:         true,
+		},
+		"Invalid-feature-string-with-no-featur-name": {
+			featureString:   "=true",
+			expectedName:    FeatureName(""),
+			expectedEnabled: false,
+			isError:         true,
+		},
+		"Invalid-feature-string-with-no-equals": {
+			featureString:   "openKruisetrue",
+			expectedName:    FeatureName(""),
+			expectedEnabled: false,
+			isError:         true,
+		},
+		"Valid-feature-string-with-spaces": {
+			featureString:   "   openKruise=true   ",
+			expectedName:    FeatureName("openKruise"),
+			expectedEnabled: true,
+			isError:         false,
+		},
+		"Invalid-feature-string-with-invalid-boolean-value": {
+			featureString:   "openKruise=yes",
+			expectedName:    FeatureName(""),
+			expectedEnabled: false,
+			isError:         true,
+		},
+	}
+
+	for _, tc := range testcases {
+		featureName, enabled, err := parseFeatureString(tc.featureString)
+
+		// Check for if error should not be thrown but en err is being returned and is not nil.
+		if !tc.isError && err != nil {
+			t.Fatal("Error was not thrown as intended.")
+		}
+
+		// If err is not thrown, we check for equality of feature name and its parsed value.
+		if err == nil {
+			if featureName != tc.expectedName {
+				t.Fatal("Feature name was incorrectly parsed.")
+			}
+			if enabled != tc.expectedEnabled {
+				t.Fatal("Enable value was incorrectly parsed.")
+			}
+		}
+	}
+}
+
+func TestParseFromFeatureGateString(t *testing.T) {
+
+	// Check for cases where parsing would fail first.
+	errorTestCases := map[string]string{
+		"Invalid-commas":       "enableOpenKruise=true,,",
+		"Invalid-equals":       "enableOpenKruise==true,",
+		"Empty-enable-value":   "enableOpenKruise=",
+		"Invalid-enable-value": "enableOpenKruise=yes,otherFeature=true",
+		"Invalid-comma-start":  ",enableOpenKruise=true",
+	}
+	for tc_name, tc := range errorTestCases {
+		fakeRisingWaveFeatureManager := NewRisingWaveFeatureManager()
+		if fakeRisingWaveFeatureManager.ParseFromFeatureGateString(tc) == nil {
+			t.Fatal(fmt.Sprintf("Parse error not thrown for testcase: %s", tc_name))
+		}
+	}
+
+	var fakeRisingWaveFeatureManager *FeatureManager
+
+	// Check for when feature gate string only contains features that are supported,
+	// we test by enabling all of them, in our featureGate string and check if all are enabled in list of features.
+	AllFeatureEnabledString := "feature-1=true,feature-2=TRUE,feature-3=1"
+	fakeRisingWaveFeatureManager = InitFeatureManagerWithSupportedFeatures(newRisingWaveSupportedFeatureListForTest())
+	fakeRisingWaveFeatureManager.ParseFromFeatureGateString(AllFeatureEnabledString)
+	if fakeRisingWaveFeatureManager.GetNumOfFeatures() != len(strings.Split(AllFeatureEnabledString, ",")) {
+		t.Fatal("Error in parsing the correct number of features")
+	}
+	for _, feature := range fakeRisingWaveFeatureManager.ListFeatures() {
+		if !fakeRisingWaveFeatureManager.IsFeatureEnabled(feature.Name) {
+			t.Fatal("Parsing has failed, not all features were enabled")
+		}
+	}
+
+	// Check for when feature gate string only contains features that are supported,
+	// we test by disabling all of them, in our featureGate string and check if all are disabled in list of features.
+	AllFeatureDisabledString := "feature-1=false,feature-2=FALSE,feature-3=0"
+	fakeRisingWaveFeatureManager = InitFeatureManagerWithSupportedFeatures(newRisingWaveSupportedFeatureListForTest())
+	fakeRisingWaveFeatureManager.ParseFromFeatureGateString(AllFeatureDisabledString)
+	if fakeRisingWaveFeatureManager.GetNumOfFeatures() != len(strings.Split(AllFeatureDisabledString, ",")) {
+		t.Fatal("Error in parsing the correct number of features")
+	}
+	for _, feature := range fakeRisingWaveFeatureManager.ListFeatures() {
+		if fakeRisingWaveFeatureManager.IsFeatureEnabled(feature.Name) {
+			t.Fatal("Parsing has failed, not all features were dsiabled")
+		}
+	}
+
+	// Check for when feature gate string also contains features that are not supported.
+	// we test by enabling all of them, in our featureGate string and check if all are enabled in list of features.
+	AllFeatureEnabledStringWithUnsupportedFeatures := fmt.Sprintf("feature-1=true,feature-2=TRUE,feature-3=1,%s=True", getNonExistentFeatureName())
+	fakeRisingWaveFeatureManager = InitFeatureManagerWithSupportedFeatures(newRisingWaveSupportedFeatureListForTest())
+	fakeRisingWaveFeatureManager.ParseFromFeatureGateString(AllFeatureEnabledStringWithUnsupportedFeatures)
+	for _, feature := range fakeRisingWaveFeatureManager.ListFeatures() {
+		if !fakeRisingWaveFeatureManager.IsFeatureEnabled(feature.Name) {
+			t.Fatal("Parsing has failed, not all features were enabled")
+		}
+	}
+}

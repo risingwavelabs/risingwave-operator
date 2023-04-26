9b72ad2e79bc4f47124381bac7bc731843705a02
arkbriar <arkbriar@gmail.com>
2023-04-27 00:53:01 +0800

    fix: try priv
    Signed-off-by: arkbriar <arkbriar@gmail.com>

--- ci/run-script.sh
+++ ci/run-script.sh
@@ -2,4 +2,4 @@
 
 set -euo pipefail
 
-docker run --rm -v "$(pwd):/workspace" -w /workspace -v /var/run/docker.sock:/var/run/docker.sock --network host --entrypoint bash "${CI_BASE_IMAGE}" "$@"
+docker run --rm --userns=host --privileged -v "$(pwd):/workspace" -w /workspace -v /var/run/docker.sock:/var/run/docker.sock --network host --entrypoint bash "${CI_BASE_IMAGE}" "$@"

b663d8a31a55823123812fe236e40beb62cf489e
arkbriar <arkbriar@gmail.com>
2023-04-27 12:14:54 +0800

    ci: fix e2e and run script
    Signed-off-by: arkbriar <arkbriar@gmail.com>

--- ci/run-script.sh
+++ ci/run-script.sh
@@ -2,6 +2,34 @@
 
 set -euo pipefail
 
-mkdir -p ci/run && env | grep "BUILDKITE\|CI\|AWS"> ci/run/env.list
+# Create a directory to store the environment variables
+mkdir -p ci/run && env | grep "BUILDKITE\|CI\|AWS" >ci/run/env.list
 
-docker run --rm --userns=host --privileged --env-file ci/run/env.list -v "$(pwd):/workspace" -w /workspace -v /var/run/docker.sock:/var/run/docker.sock --network host --entrypoint bash "${CI_BASE_IMAGE}" "$@"
+# Get environment variables from the command line
+env_args=()
+while getopts ":e:" opt; do
+  case ${opt} in
+  e)
+    [[ -v "${OPTARG}" ]] && env_args+=("-e" "${OPTARG}")
+    ;;
+  \?)
+    echo "Invalid option: $OPTARG" 1>&2
+    exit 1
+    ;;
+  :)
+    echo "Invalid option: $OPTARG requires an argument" 1>&2
+    exit 1
+    ;;
+  esac
+done
+
+# Get the arguments after the options
+shift $((OPTIND - 1))
+
+# shellcheck disable=SC2068
+docker run --rm --userns=host --privileged --network host \
+  ${env_args[@]} --env-file ci/run/env.list \
+  -v "$(pwd):/workspace" -w /workspace \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --entrypoint bash \
+  "${CI_BASE_IMAGE}" "$@"

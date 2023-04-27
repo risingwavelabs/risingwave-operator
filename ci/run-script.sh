8a654149184f38c43507bf53d271e1d45ff2f16a
arkbriar <arkbriar@gmail.com>
2023-04-27 11:45:17 +0800

    ci: fix dockerfile and scripts
    Signed-off-by: arkbriar <arkbriar@gmail.com>

--- ci/run-script.sh
+++ ci/run-script.sh
@@ -2,4 +2,6 @@
 
 set -euo pipefail
 
-docker run --rm --userns=host --privileged -v "$(pwd):/workspace" -w /workspace -v /var/run/docker.sock:/var/run/docker.sock --network host --entrypoint bash "${CI_BASE_IMAGE}" "$@"
+mkdir -p ci/run && env | grep "BUILDKITE\|CI\|AWS"> ci/run/env.list
+
+docker run --rm --userns=host --privileged --env-file ci/run/env.list -v "$(pwd):/workspace" -w /workspace -v /var/run/docker.sock:/var/run/docker.sock --network host --entrypoint bash "${CI_BASE_IMAGE}" "$@"

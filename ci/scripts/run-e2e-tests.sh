0a13eac3d2bee253f93e43314f5aaea800b8168e
arkbriar <arkbriar@gmail.com>
2023-11-03 17:11:20 +0800

    fix ci scripts
    Signed-off-by: arkbriar <arkbriar@gmail.com>

--- ci/scripts/run-e2e-tests.sh
+++ ci/scripts/run-e2e-tests.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
+if [[ "$CI_ENV" = "1" ]]; then
+  echo "--- Mark /workspace as safe directory (VCS)"
+  git config --global --add safe.directory /workspace
+fi
+
 echo "--- Ensure that the kind cluster is deleted"
 
 kind delete cluster --name e2e

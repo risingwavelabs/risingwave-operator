bb86accc05f540df5ab114199fbf67fe8fa33675
Stab123 <55347567+Stab123@users.noreply.github.com>
2024-05-13 06:43:13 +0000

    chore: bump risingwave image tags to v1.8.4

--- test/e2e/tests/tests.sh
+++ test/e2e/tests/tests.sh
@@ -15,7 +15,7 @@
 ${__E2E_SOURCE_TESTS_TESTS_SH__:=false} && return 0 || __E2E_SOURCE_TESTS_TESTS_SH__=true
 
 export E2E_RISINGWAVE_NAME="${E2E_RISINGWAVE_NAME:=e2e}"
-export E2E_RISINGWAVE_IMAGE="${E2E_RISINGWAVE_IMAGE:=risingwavelabs/risingwave:v1.8.2}"
+export E2E_RISINGWAVE_IMAGE="${E2E_RISINGWAVE_IMAGE:=risingwavelabs/risingwave:v1.8.4}"
 
 source "$(dirname "${BASH_SOURCE[0]}")/risingwave/tests.sh"
 source "$(dirname "${BASH_SOURCE[0]}")/risingwavescaleview/tests.sh"

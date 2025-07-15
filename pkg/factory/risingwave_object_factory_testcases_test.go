7048e963714b7775f781e0dad559dbafb7f037d7
Luisniel <lgarcia@kinxpro.com>
2025-07-15 00:17:40 +0000

    test: add s3-compatible-path-style test case

--- pkg/factory/risingwave_object_factory_testcases_test.go
+++ pkg/factory/risingwave_object_factory_testcases_test.go
@@ -3282,6 +3282,71 @@ func stateStoreTestCases() map[string]stateStoresTestCase {
 				},
 			},
 		},
+		"s3-compatible-path-style": {
+			stateStore: risingwavev1alpha1.RisingWaveStateStoreBackend{
+				S3: &risingwavev1alpha1.RisingWaveStateStoreBackendS3{
+					Bucket:         "s3-hummock01",
+					Endpoint:       "https://oss-cn-hangzhou.aliyuncs.com",
+					ForcePathStyle: true,
+					RisingWaveS3Credentials: risingwavev1alpha1.RisingWaveS3Credentials{
+						SecretName:         "s3-creds",
+						AccessKeyRef:       consts.SecretKeyAWSS3AccessKeyID,
+						SecretAccessKeyRef: consts.SecretKeyAWSS3SecretAccessKey,
+					},
+				},
+			},
+			envs: []corev1.EnvVar{
+				{
+					Name:  "RW_STATE_STORE",
+					Value: "hummock+s3://s3-hummock01",
+				},
+				{
+					Name:  "AWS_S3_BUCKET",
+					Value: "s3-hummock01",
+				},
+				{
+					Name:  "RW_S3_ENDPOINT",
+					Value: "https://oss-cn-hangzhou.aliyuncs.com",
+				},
+				{
+					Name:  "RW_IS_FORCE_PATH_STYLE",
+					Value: "true",
+				},
+				{
+					Name: "AWS_ACCESS_KEY_ID",
+					ValueFrom: &corev1.EnvVarSource{
+						SecretKeyRef: &corev1.SecretKeySelector{
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: "s3-creds",
+							},
+							Key: consts.SecretKeyAWSS3AccessKeyID,
+						},
+					},
+				},
+				{
+					Name: "AWS_SECRET_ACCESS_KEY",
+					ValueFrom: &corev1.EnvVarSource{
+						SecretKeyRef: &corev1.SecretKeySelector{
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: "s3-creds",
+							},
+							Key: consts.SecretKeyAWSS3SecretAccessKey,
+						},
+					},
+				},
+				{
+					Name: "AWS_REGION",
+					ValueFrom: &corev1.EnvVarSource{
+						SecretKeyRef: &corev1.SecretKeySelector{
+							LocalObjectReference: corev1.LocalObjectReference{
+								Name: "s3-creds",
+							},
+							Key: consts.SecretKeyAWSS3Region,
+						},
+					},
+				},
+			},
+		},
 		"s3-with-region": {
 			stateStore: risingwavev1alpha1.RisingWaveStateStoreBackend{
 				S3: &risingwavev1alpha1.RisingWaveStateStoreBackendS3{
